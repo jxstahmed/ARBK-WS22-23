22336bc2b3eb7dd7b060b0c0e2b4412f068b1a85
Ahmed J <103430690+jxstahmed@users.noreply.github.com>
2022-11-01 23:53:43 +0100

    Add files via upload

---
+++ ARBKVS_Aufgabe2/Interrupt/Interrupt/main.cpp
@@ -0,0 +1,78 @@
+/*
+ * ARBKVS_Aufgabe2.cpp
+ *
+ * Created: 28.10.2022 11:28:31
+ * Author : Ahmed
+ */ 
+
+#include <avr/io.h>
+
+#define F_CPU 16000000UL
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <stdbool.h>										// Um Boolean zunutzen [überprüfung ob LED blinkt]
+
+
+#define SW1 PIND2											// Auf den Ports PIND2 [INT0] und PIND3 [INT1] liegen die EICRA [Register für externe Interrupts].
+#define SW2 PIND3
+#define LED0 PINB0
+#define LED9 PINB1
+
+volatile int an = LED0;										// volatile -> Sagt dem Compiler das er diese Variable nicht optimieren oder ändern soll.
+volatile bool blinkt = true;
+
+void ports_einrichten()
+{
+	DDRB = 0xff;											// Wir setzten die Ports B0 und B1 als Ausgänge für die LEDs.
+	PORTB = 0x00;
+	
+	DDRD = 0x00;											// Wir setzten das DDRD als Eingang für die Taster.
+	PORTD |= (1 << SW1) | (1 << SW2);						// Wir setzen die beiden Taster als Ausgänge, um sie nutzen zu können.
+	EICRA |= (1 << ISC11) | (1 << ISC01);					// Wir legen die Bedingung fest, dass bei einer fallenden Flanke ein Interrupt Request generiert wird. [Wird im Interrupt Sense Control gespeichert]
+	EIMSK = (1 << INT0) | (1 << INT1);						// Wir setzen die beiden INT-Bits und geben die beiden externen Interrupts frei.
+	sei();													// Wir aktivieren die Interrupts, durch das setzten der globalen Interrupt Maske. [standard funktion]
+}
+
+ISR(INT0_vect)										// Bei dieser Funktion handelt es sich um die Interrupt Routinen [Was genau getan werden soll, bei einem Request]
+{
+	if (an == LED0)									// Wenn die LED0 bereits an ist, so wird der Status der LED geändert. [Blinkt -> Leuchten, oder Leuchten -> Blinkt]
+	{										
+		blinkt = !blinkt;
+	} 
+	else											// Wenn die LED0 nicht an ist, so ist die LED9 an. Somit schalten wir die LED9 aus und die LED0 an.
+	{
+		PORTB = (1 << LED0);
+	}
+	an = LED0;
+}
+
+ISR(INT1_vect)
+{
+	if (an == LED9)									// Wenn die LED9 bereits an ist, so wird der Status der LED geändert. [Blinkt -> Leuchten, oder Leuchten -> Blinkt]
+	{
+		blinkt = !blinkt;
+	} 
+	else											// Wenn die LED9 nicht an ist, so ist die LED0 an. Somit schalten wir die LED0 aus und die LED9 an.
+	{
+		PORTB = (1 << LED9);
+	}
+	an = LED9;
+}
+
+
+int main(void)
+{
+	ports_einrichten();								// Wir richten alle PORTS ein, um starten zu können.
+	while(1)										// Geschwindigkeit des Blinkens
+	{
+		if (blinkt)
+		{
+			PORTB ^= (1 << an);						
+		} 
+		else
+		{
+			PORTB = (1 << an);
+		}
+		_delay_ms(200);
+	}
+}
---
+++ ARBKVS_Aufgabe2/Polling/Polling/main.cpp
@@ -0,0 +1,87 @@
+/*
+ * ARBKVS_Aufgabe2.cpp
+ *
+ * Created: 28.10.2022 11:28:09
+ * Author : Ahmed
+ */ 
+
+#include <avr/io.h>
+#define F_CPU 16000000UL
+#include <util/delay.h>
+#include <stdbool.h>									// Um Boolean nutzen zu können.
+
+#define SW1 PINB0
+#define SW2 PINB1
+#define LED0 PIND0
+#define LED9 PIND1
+
+int an = LED0;
+int aus = LED9;
+bool blinkt = true;
+
+void ports_einrichten()
+{
+	DDRD = 0xff;										// Wir setzen das DDRD als Ausgang.
+	PORTD = 0x00;
+	
+	DDRB = 0x00;										// Wir setzen das DDRB als Eingang.
+	PORTB = 0xff;
+}
+
+void taster()
+{
+	if(!(PINB & (1 << SW1)))						// (PINB & (1 << SW1) -> Check ob High oder Low, das ! sagt somit bei Low
+	{
+		if(PIND & (1 << LED0))						// Wenn die LED0 an ist
+		{
+			blinkt ^= 1;							// Wir ändern den Status der LED [Blinken -> Leuchten oder Leuchten -> Blinken]
+		}
+		else if(PIND & (1 << LED9))					// Wenn die LED9 an ist, schalten wir um auf die LED0.
+		{
+			aus = LED9;
+			an = LED0;
+		}
+	}
+	
+	if(!(PINB & (1 << SW2)))
+	{
+		if(PIND & (1 << LED9))						// Wenn die LED9 an ist
+		{
+			blinkt ^= (1 << 0);						// Wir ändern den Status der LED [Blinken -> Leuchten oder Leuchten -> Blinken]
+		}
+		else if((PIND & (1 << LED0)))				// Wenn die LED0 an ist, schalten wir um auf die LED9.
+		{
+			aus = LED0;
+			an = LED9;
+		}
+	}
+}
+
+int main(void)
+{
+	ports_einrichten();
+	while (1)
+	{
+		taster();
+		if (blinkt)
+		{
+			PORTD &= ~(1UL << aus);						// Ich nutze UL statt int, da bei berechnungen sonst Fehler auftreten. [Manchmal leuchten beide LEDs beim wechseln]
+			PORTD ^= (1 << an);
+		}
+		else
+		{
+			PORTD &= ~(1UL << aus);
+			PORTD |= ( 1 << an);
+		}
+		_delay_ms(200);
+	}
+}
+
+/*
+
+^= -> XOR
+&= -> AND
+|= -> OR
+~ -> NOT
+
+*/
